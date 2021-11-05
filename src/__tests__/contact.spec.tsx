f42e76093c5cfcee23beee114b338c9e8eb9dc20
alexandre jeanjean <ajeanjean@simplonprod.co>
2021-11-05 16:41:39 +0100

    Add enzyme configuration

---
+++ src/__tests__/contact.spec.tsx
@@ -0,0 +1,12 @@
+
+import { mount } from 'enzyme';
+import Contact from "../components/Contact";
+
+/** @test {Contact Component} */
+describe('Contact Component', () => {
+    it('should render without crashing', () => {
+        const wrapper = mount(<Contact name="Alex Jeanjean" />);
+
+        expect(wrapper.find('p')).toHaveLength(1);
+    });
+});
